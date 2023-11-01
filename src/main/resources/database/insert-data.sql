fce4a6a117b1f9ec0e428a1fecc3570c1803b182
Javier Roig Doménech <jroigdo@mercadona.es>
2023-11-01 13:44:52 +0100

    change sql script

--- src/main/resources/database/insert-data.sql
+++ src/main/resources/database/insert-data.sql
@@ -1,8 +1,8 @@
 insert into "user" ("id", "enabled", "password", "username", "created_by", "created_date") values (1, true, '$2a$10$ucU0Td/bQ5xWfFK5KRfdw.8D1YHxX3rx1HrCL6Nk16Tfw4UK/bfHO', 'admin', 'admin', now()) on conflict ("id") do nothing;
 insert into "user" ("id", "enabled", "password", "username", "created_by", "created_date") values (2, true, '$2a$10$ucU0Td/bQ5xWfFK5KRfdw.8D1YHxX3rx1HrCL6Nk16Tfw4UK/bfHO', 'jroigdo', 'admin', now()) on conflict ("id") do nothing;
 
-insert into "role" ("id", "enabled", "name", "created_by", "created_date") values (1, true, 'ADMIN', 'admin', now()) on conflict ("id") do nothing;
-insert into "role" ("id", "enabled", "name", "created_by", "created_date") values (2, true, 'USER', 'admin', now()) on conflict ("id") do nothing;
+insert into "role" ("id", "enabled", "name", "created_by", "created_date") values (1, true, 'admin', 'admin', now()) on conflict ("id") do nothing;
+insert into "role" ("id", "enabled", "name", "created_by", "created_date") values (2, true, 'user', 'admin', now()) on conflict ("id") do nothing;
 
 insert into "user_role" ("id", "enabled", "created_by", "created_date", "user_id", "role_id") values (1, true, 'admin', now(), 1, 1) on conflict ("id") do nothing;
 insert into "user_role" ("id", "enabled", "created_by", "created_date", "user_id", "role_id") values (2, true, 'admin', now(), 2, 2) on conflict ("id") do nothing;
